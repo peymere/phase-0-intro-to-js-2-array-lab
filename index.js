0aa361d63be1d2fee58b8aae9ac4adce4047b43d
Peyton Meredith <peymeredith@gmail.com>
2023-08-21 21:02:16 -0500

    arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+//const appendCat = [...cats, "Broom"]
+//console.log(cats)
+//console.log(appendCat)
+
+function appendCat(){
+   return [...cats, "Broom"]
+}
+
+function prependCat(){
+    return ["Arnold", ...cats]
+}
+
+function removeLastCat(){
+    return cats.slice(0, -1)
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
